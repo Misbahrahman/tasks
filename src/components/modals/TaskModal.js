1c66f0f66548dac5b47d5f36b5c1beda3d35822b
Misbahrahman <mrahman7749@gmail.com>
2025-01-11 13:53:15 +0530

    no change

--- src/components/modals/TaskModal.js
+++ src/components/modals/TaskModal.js
@@ -73,6 +73,7 @@
     };
 
     if (!isOpen) return null;
+    //absolutely no change
 
     return (
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
